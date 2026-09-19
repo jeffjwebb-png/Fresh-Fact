0ba8bd272f039f9b66140e3a3d057d895970d63c
jeffjwebb-png <jeffjwebb@gmail.com>
2026-09-18 20:30:05 -0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,1 +1,1 @@
-const express = require(“express”); const app = express(); const port = process.env.PORT || 3000; app.get(”/”, (req, res) => { res.json({ message: “FreshFact API is live” }); }); port, () => { console.log(FreshFact running on port ${port})
+const express = require(‘express’); const app = express(); const port = process.env.PORT || 3000; app.get(’/’, (req, res) => { res.json({ message: ‘FreshFact API is live’ }); }); app.listen(port, () => { console.log(‘FreshFact API is running’); 
